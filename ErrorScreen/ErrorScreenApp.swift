d69c0f27a1907ec52e5d6fc51664217dc1eda037
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-08 17:44:08 +0700

    Create the project

---
+++ ErrorScreen/ErrorScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorScreenApp.swift
+//  ErrorScreen
+//
+//  Created by Danh Tu on 08/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ErrorScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
